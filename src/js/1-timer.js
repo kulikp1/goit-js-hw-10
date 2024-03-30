b98c6c6a7731726790718893bbd8c0e9cc1cbe16
kulikp1 <pashakylik2@gmail.com>
2024-03-30 11:25:58 +0200

    do timer and snackbar

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,84 @@
-console.log(timer);
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+// Пошук елементів за айдішником, запис їх у змінні
+const datetimePicker = document.getElementById('datetime-picker');
+const startBtn = document.getElementById('start-btn');
+const daysSpan = document.getElementById('days');
+const hoursSpan = document.getElementById('hours');
+const minutesSpan = document.getElementById('minutes');
+const secondsSpan = document.getElementById('seconds');
+
+// Оголошення змінних для таймера
+let countdownInterval;
+let userSelectedDate;
+startBtn.disabled = true;
+
+// Ініціалізація календаря flatpickr
+flatpickr(datetimePicker, {
+  enableTime: true,
+  time_24hr: true,
+  minuteIncrement: 1,
+  onClose: function(selectedDates) {
+    if (selectedDates[0] > new Date()) {
+      startBtn.disabled = false; 
+        userSelectedDate = selectedDates[0]; // Збереження обраної дати
+        iziToast.success({
+    title: '',
+    message: 'Дата вибрана, клацай старт!',
+});
+    } else {
+      startBtn.disabled = true; 
+      iziToast.error({
+        title: '',
+        message: 'Будь ласка, обери дату в майбутньому',
+      });
+    }
+  },
+});
+
+// Функція підрахунку часу до кінцевої дати
+function calculateTimeRemaining() {
+  const now = new Date().getTime();
+  const difference = userSelectedDate - now;
+
+  if (difference <= 0) {
+    clearInterval(countdownInterval);
+    return;
+  }
+
+  const { days, hours, minutes, seconds } = convertMs(difference);
+
+  // Оновлення таймера
+  daysSpan.textContent = addZero(days);
+  hoursSpan.textContent = addZero(hours);
+  minutesSpan.textContent = addZero(minutes);
+  secondsSpan.textContent = addZero(seconds);
+}
+
+// Функція для додавання нуля перед числом, якщо воно має менше двох символів
+function addZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+startBtn.addEventListener('click', function() {
+  countdownInterval = setInterval(calculateTimeRemaining, 1000); 
+  startBtn.disabled = true; 
+});
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,33 @@
-console.log(snackbar);
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const form = document.querySelector(".form");
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  const delay = parseInt(this.elements['delay'].value);
+  const state = this.elements['state'].value;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+
+  promise.then(delay => {
+    iziToast.success({
+      title: 'Success',
+      message: `✅ Fulfilled promise in ${delay}ms`,
+    });
+  }).catch(delay => {
+    iziToast.error({
+      title: 'Error',
+      message: `❌ Rejected promise in ${delay}ms`,
+    });
+  });
+});
+
